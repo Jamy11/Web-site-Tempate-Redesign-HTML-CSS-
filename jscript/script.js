acd9e7fdf63cf56bf42d4315b716667c7165f6af
Jamy Ar Rahman <jamyarrahman82@gmail.com>
2020-01-10 22:32:38 +0600

    All Done

---
+++ jscript/script.js
@@ -0,0 +1,120 @@
+$(document).ready(function(){
+    $("#show1").click(function(){
+        $(".ptag1").toggle();
+	});
+	$("#show1").click(function(){
+		$("#show1").hide();
+ });
+	$("#hide1").click(function(){
+		$("#hide1").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide1").click(function(){
+		$(".ptag1").toggle();
+		$("#show1").show();
+	});
+});
+});
+
+$(document).ready(function(){
+    $("#show2").click(function(){
+        $(".ptag2").toggle();
+	});
+	$("#show2").click(function(){
+		$("#show2").hide();
+ });
+	$("#hide2").click(function(){
+		$("#hide2").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide2").click(function(){
+		$(".ptag2").toggle();
+		$("#show2").show();
+	});
+});
+});
+
+
+
+$(document).ready(function(){
+    $("#show3").click(function(){
+        $(".ptag3").toggle();
+	});
+	$("#show3").click(function(){
+		$("#show3").hide();
+ });
+	$("#hide3").click(function(){
+		$("#hide3").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide3").click(function(){
+		$(".ptag3").toggle();
+		$("#show3").show();
+	});
+});
+});
+
+
+$(document).ready(function(){
+    $("#show4").click(function(){
+        $(".ptag4").toggle();
+	});
+	$("#show4").click(function(){
+		$("#show4").hide();
+ });
+	$("#hide4").click(function(){
+		$("#hide4").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide4").click(function(){
+		$(".ptag4").toggle();
+		$("#show4").show();
+	});
+});
+});
+
+
+$(document).ready(function(){
+    $("#show5").click(function(){
+        $(".ptag5").toggle();
+	});
+	$("#show5").click(function(){
+		$("#show5").hide();
+ });
+	$("#hide5").click(function(){
+		$("#hide5").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide5").click(function(){
+		$(".ptag5").toggle();
+		$("#show5").show();
+	});
+});
+});
+
+
+
+$(document).ready(function(){
+    $("#show6").click(function(){
+        $(".ptag6").toggle();
+	});
+	$("#show6").click(function(){
+		$("#show6").hide();
+ });
+	$("#hide6").click(function(){
+		$("#hide6").show();
+	});
+	
+$(document).ready(function(){
+    $("#hide6").click(function(){
+		$(".ptag6").toggle();
+		$("#show6").show();
+	});
+});
+});
+
